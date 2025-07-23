8dfeaa38961e9937c73000dd3a42be6a223e5e26
MATHEUS SOUZA DE FIGUEIREDO ARAÚJO <matheus.araujo4950208@edu.pe.senac.br>
2025-07-23 10:32:11 -0300

    Codigo que está relacionado aos operadores if e else

---
+++ codigo.java
@@ -0,0 +1,24 @@
+package atvidade_IF;
+
+import java.util.Scanner;
+
+public class atv_05 {
+	public static void main(String[] args) {
+		java.util.Scanner nota = new java.util.Scanner(System.in);
+		
+		/*Crie um programa que leia a nota de um aluno e verifique se ele está aprovado
+		(nota maior ou igual a 6) ou reprovado.*/
+		
+		System.out.println("Qual foi a sua nota final:? ");
+		float num = nota.nextFloat();
+		
+		if (num >= 6) {
+			System.out.println("Parabéns! você está aprovado ");
+			
+		}else {
+			System.out.println("BUÁ BUÁ, você está reprovado ");
+			
+			nota.close();
+		}
+	}
+}

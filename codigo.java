2ef14355e19aa9c9ecb5e84356cbf2fa948a9d02
Renan <renan.lima4946381@edu.pe.senac.br>
2025-07-23 12:14:18 -0300

    Nova alteração com elif e comentários

--- codigo.java
+++ codigo.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class atv_05 {
 	public static void main(String[] args) {
 		java.util.Scanner nota = new java.util.Scanner(System.in);
-		
-		/*Crie um programa que leia a nota de um aluno e verifique se ele está aprovado
-		(nota maior ou igual a 6) ou reprovado.*/
-		
+
 		System.out.println("Qual foi a sua nota final:? ");
 		float num = nota.nextFloat();
 		
-		if (num >= 6) {
+		if (num >= 7) {
 			System.out.println("Parabéns! você está aprovado ");
-			
+
+		//Colocando mais uma opção que possibilite a realização de uma recuperação ao aluno com nota maior que 4
+
+		}elif (num > 4) {
+			System.out.println("Você está em recuperação.")
+		
+		//Se a nota for menor que 4 aí ele estará em recuperação
+
 		}else {
 			System.out.println("BUÁ BUÁ, você está reprovado ");
 			
